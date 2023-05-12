6af34665d18e93cceefe50ed7975e20453b40a57
Mario vg <22670080@itiguala.edu.mx>
2023-05-12 07:50:40 -0600

    Creacion de la clase ArrayType

--- src/Arreglos/ArrayType.java
+++ src/Arreglos/ArrayType.java
@@ -4,14 +4,15 @@ public class ArrayType {
     public static void main(String[] args) {
             String [] bugs = {"cricket", "beetle", "ladybug"};
             String [] alias = bugs;
-        System.out.println(bugs.equals(alias)); //true
+        System.out.println(bugs.equals(alias));
         System.out.println(bugs.toString()); // [Ljava.lang.String;@4dd8dc3
         System.out.println(bugs[0]);
         System.out.println(bugs[1]);
         System.out.println(bugs[2]);
         System.out.println("\n");
         for (int i=0; i<bugs.length; i++){
-            System.out.println(bugs[i]); //imprime todo bugs sin necesidad de tanto sout
+            System.out.println(bugs[i]);
+
         }
     }
 }
